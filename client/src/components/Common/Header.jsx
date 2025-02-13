18875458a2a70f625215784f924e070ccf058eb0
ABHI <abhi202864@gmail.com>
2025-02-13 23:52:31 +0530

    add navbar

--- client/src/components/Common/Header.jsx
+++ client/src/components/Common/Header.jsx
@@ -1,14 +1,14 @@
 import TopBar from "../Layout/TopBar";
+import Navbar from "./Navbar";
 
 
 const Header = () => {
   return (
-    <div>
-      {/* TOP-BAR */}
+    <header>
       <TopBar />
-      {/* NAV-BAR */}
+      <Navbar />
       {/* CART-DRAWER */}
-    </div>
+    </header>
   )
 }
 
