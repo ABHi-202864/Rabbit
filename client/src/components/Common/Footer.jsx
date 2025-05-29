655ff6c8aee2ee5c275000a8ec47333835dea776
ABHI <abhi202864@gmail.com>
2025-05-29 23:42:07 +0530

    add hero section

--- client/src/components/Common/Footer.jsx
+++ client/src/components/Common/Footer.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+
+import { Link } from 'react-router-dom';
+import { TbBrandMeta } from "react-icons/tb";
+import { IoLogoInstagram } from "react-icons/io5";
+import { RiTwitterXLine } from "react-icons/ri";
+import { FiPhoneCall } from "react-icons/fi"
 
 function Footer() {
   return (
     <>
       <footer className='border-t py-12'>
-        <div
-          className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4"
-        >
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 px-4">
           <div>
             <h3 className='text-lg text-gray-800 mb-4'>Newsletter</h3>
             <p className='text-gray-500 mb-4'>
@@ -31,9 +34,83 @@ function Footer() {
           </div>
 
           {/* SHOP LINKS */}
-          <div>
-            <h3 className="text-lg"></h3>
+          <div className='md:ml-20'>
+            <h3 className="text-lg text-gray-800 mb-4">Shop</h3>
+            <ul className='space-y-2 text-gray-600 transition-colors'>
+              <li>
+                <Link to="#">Men&apos;s Top Wear</Link>
+              </li>
+              <li>
+                <Link to="#">Women&apos;s Top Wear</Link>
+              </li>
+              <li>
+                <Link to="#">Men&apos;s Bottom Wear</Link>
+              </li>
+              <li>
+                <Link to="#">Wemen&apos;s Bottom Wear</Link>
+              </li>
+            </ul>
           </div>
+
+          {/* SUPPORT LINKS */}
+          <div className='md:ml-20'>
+            <h3 className="text-lg text-gray-800 mb-4">Support</h3>
+            <ul className='space-y-2 text-gray-600 transition-colors'>
+              <li>
+                <Link to="#">Contact Us</Link>
+              </li>
+              <li>
+                <Link to="#">About Us</Link>
+              </li>
+              <li>
+                <Link to="#">FAQs</Link>
+              </li>
+              <li>
+                <Link to="#">Features</Link>
+              </li>
+            </ul>
+          </div>
+
+          {/* FOLLOW US */}
+          <div className='md:ml-14'>
+            <h3 className="text-lg text-gray-800 mb-4">Follow Us</h3>
+            <div className="flex items-center space-x-4 mb-6">
+              <a
+                href="https://www.facebook.com"
+                target='_blank'
+                rel='noopener noreferrer'
+                className='hover:text-gray-500'
+              >
+                <TbBrandMeta className='h-5 w-5' />
+              </a>
+              <a
+                href="https://www.facebook.com"
+                target='_blank'
+                rel='noopener noreferrer'
+                className='hover:text-gray-500'
+              >
+                <IoLogoInstagram className="h-5 w-5" />
+              </a>
+              <a
+                href="https://www.facebook.com"
+                target='_blank'
+                rel='noopener noreferrer'
+                className='hover:text-gray-500'
+              >
+                <RiTwitterXLine className="h-5 w-5" />
+              </a>
+            </div>
+            <p className='text-gray-500'>Call Us</p>
+            <p>
+              <FiPhoneCall className='inline-block mr-2' />
+              0123-456-789
+            </p>
+          </div>
+        </div>
+
+        {/* FOOTER BOTTOM */}
+        <div className='container mx-auto mt-12 px-4 lg:px-0 border-t border-gray-200 pt-6'>
+          <p className='text-gray-500 text-sm tracking-tighter text-center'>&copy;2025, CompileTab. All Right Reserved.</p>
         </div>
       </footer>
     </>
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+
+import Hero from '../components/Layout/Hero';
+import GenderCollectionSection from '../components/Products/GenderCollectionSection';
+
+function Home() {
+  return (
+    <>
+      <Hero />
+      <GenderCollectionSection />
+    </>
+  )
+}
+
+export default Home;
