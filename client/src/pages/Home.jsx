965ef76ade9cfe87271690e5d4c8c65d6acef931
ABHI <abhi202864@gmail.com>
2025-05-31 23:11:19 +0530

    add slide arrow logic

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 
 import Hero from '../components/Layout/Hero';
 import GenderCollectionSection from '../components/Products/GenderCollectionSection';
+import NewArrivals from '../components/Products/NewArrivals';
 
 function Home() {
   return (
     <>
       <Hero />
       <GenderCollectionSection />
+      <NewArrivals />
     </>
   )
 }
