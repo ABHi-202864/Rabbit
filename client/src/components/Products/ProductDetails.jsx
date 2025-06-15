c82eb47431b73be6b45655dd8b1981fe1b65b082
ABHI <abhi202864@gmail.com>
2025-06-15 22:38:39 +0530

    add add to code functionality code

--- client/src/components/Products/ProductDetails.jsx
+++ client/src/components/Products/ProductDetails.jsx
@@ -1,4 +1,5 @@
-
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
 
 const selectedProduct = {
   name: "Stylish Jacket",
@@ -22,6 +23,40 @@ const selectedProduct = {
 }
 
 function ProductDetails() {
+  const [mainImage, setMainImage] = useState();
+  const [selectedSize, setSelectedSize] = useState();
+  const [selectedColor, setSelectedColor] = useState();
+  const [quantity, setQuantity] = useState(1);
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  useEffect(() => {
+    if (selectedProduct?.images?.length > 0) {
+      setMainImage(selectedProduct.images[0].url);
+    }
+  }, [selectedProduct]);
+
+  const handleQuantityChange = (action) => {
+    if (action === "plus") setQuantity((prev) => prev + 1);
+    if (action === "minus" && quantity > 1) setQuantity((prev) => prev - 1);
+  }
+
+  const handleAdToCart = () => {
+    if (!selectedSize || !selectedColor) {
+      toast.error("Please select a size and color before adding to cart.", {
+        duration: 3000,
+      });
+
+      return;
+    }
+
+    setIsButtonDisabled(true);
+
+    setTimeout(() => {
+      toast.success("Product added to cart!", { duration: 3000 });
+      setIsButtonDisabled(false);
+    }, 500);
+  }
+
   return (
     <>
       <div className="p-6">
@@ -31,10 +66,11 @@ function ProductDetails() {
             <div className="hidden md:flex flex-col space-y-4 mr-6">
               {selectedProduct.images.map((item, index) => (
                 <img
+                  onClick={() => setMainImage(item.url)}
                   key={index}
                   src={item.url}
                   alt={item.altText || `Thumbnail ${index}`}
-                  className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+                  className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${mainImage === item.url ? "border-2 border-black" : "border-2 border-gray-300"}`}
                 />
               ))}
             </div>
@@ -43,7 +79,7 @@ function ProductDetails() {
             <div className="md:w-1/2">
               <div className="mb-4">
                 <img
-                  src={selectedProduct.images[0]?.url}
+                  src={mainImage}
                   alt="Main Product"
                   className="w-full h-auto object-center rounded-lg"
                 />
@@ -54,12 +90,13 @@ function ProductDetails() {
             <div className="md:hidden flex overscroll-x-scroll space-x-4 mb-4">
               {selectedProduct.images.map((item, index) => (
                 <img
+                  onClick={() => setMainImage(item.url)}
                   key={index}
                   src={item.url}
                   alt={item.altText || `Thumbnail ${index}`}
-                  className="w-20 h-20 object-cover rounded-lg cursor-pointer border"
+                  className={`w-20 h-20 object-cover rounded-lg cursor-pointer border ${mainImage === item.url ? "border-2 border-black" : "border-2 border-gray-300"}`}
                 />
-              ))}-
+              ))}
             </div>
 
             {/* Right Side */}
@@ -80,12 +117,13 @@ function ProductDetails() {
               <div className="mb-4">
                 <p className="text-gray-700">Color:</p>
                 <div className="flex gap-2 mt-2">
-                  {selectedProduct.colors.map((item, index) => (
+                  {selectedProduct.colors.map((collor) => (
                     <button
-                      key={index}
-                      className="w-8 h-8 rounded-full border cursor-pointer"
+                      key={collor}
+                      onClick={() => setSelectedColor(collor)}
+                      className={`w-8 h-8 rounded-full border cursor-pointer ${selectedColor === collor ? "border-4 border-black" : "border-gray-300"}`}
                       style={{
-                        backgroundColor: item.toLocaleLowerCase(),
+                        backgroundColor: collor.toLocaleLowerCase(),
                         filter: "brightness(0.5)"
                       }}
                     >
@@ -97,9 +135,13 @@ function ProductDetails() {
               <div className="mb-4">
                 <p className="text-gray-700">Size:</p>
                 <div className="flex gap-2 mt-2">
-                  {selectedProduct.sizes.map((item) => (
-                    <button key={item} className="px-4 py-2 rounded border cursor-pointer">
-                      {item}
+                  {selectedProduct.sizes.map((size) => (
+                    <button
+                      key={size}
+                      onClick={() => setSelectedSize(size)}
+                      className={`px-4 py-2 rounded border cursor-pointer ${selectedSize === size ? "bg-black text-white" : ""}`}
+                    >
+                      {size}
                     </button>
                   ))}
                 </div>
@@ -107,9 +149,47 @@ function ProductDetails() {
 
               <div className="mb-6">
                 <p className="text-gray-700">Quantity:</p>
-                <div className=""></div>
+                <div className="flex items-center space-x-4 mt-2">
+                  <button
+                    onClick={() => handleQuantityChange("minus")}
+                    className="px-3 py-1 bg-gray-200 rounded text-lg cursor-pointer"
+                  >-</button>
+                  <span>{quantity}</span>
+                  <button
+                    onClick={() => handleQuantityChange("plus")}
+                    className="px-3 py-1 bg-gray-200 rounded text-lg cursor-pointer"
+                  >+</button>
+                </div>
+              </div>
+
+              <button
+                onClick={() => handleAdToCart()}
+                disabled={isButtonDisabled}
+                className={`bg-black text-white py-2 px-6 rounded w-full mb-4 cursor-pointer ${isButtonDisabled ? "cursor-not-allowed opacity-50" : "hover:bg-gray-900"}`}
+              >
+                {isButtonDisabled ? "Adding..." : "ADD TO CART"}
+              </button>
+
+              <div className="mt-10 text-gray-700">
+                <h3 className="text-xl font-bold mb-4">Characteristics:</h3>
+                <table className="w-full text-left text-sm text-gray-600">
+                  <tbody>
+                    <tr>
+                      <td className="py-1">Brand</td>
+                      <td className="py-1">{selectedProduct.brand}</td>
+                    </tr>
+                    <tr>
+                      <td className="py-1">Material</td>
+                      <td className="py-1">{selectedProduct.material}</td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
             </div>
+          </div>
+
+          <div className="mt-20">
+                  
           </div>
         </div>
       </div>
