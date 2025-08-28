45d43d2b0647fc1d367b7ff6dc9635b6176b5c53
ABHI <abhi202864@gmail.com>
2025-08-28 13:27:51 +0530

    add order confirmation page

--- client/src/pages/MyOrdersPage.jsx
+++ client/src/pages/MyOrdersPage.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 
 function MyOrdersPage() {
   const [orders, setOrders] = useState([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     // Simulate fetching orders
@@ -40,6 +43,9 @@ function MyOrdersPage() {
     }, 1000);
   }, []);
 
+  const handleRowClick = (orderId) => {
+    navigate(`/order/${orderId}`);
+  }
 
   return (
     <>
@@ -62,7 +68,11 @@ function MyOrdersPage() {
               {
                 orders.length > 0 ? (
                   orders.map((order) => (
-                    <tr key={order._id} className="border-b border-gray-300 hover:border-gray-50 cursor-pointer">
+                    <tr
+                      key={order._id}
+                      onClick={() => handleRowClick(order._id)}
+                      className="border-b border-gray-300 hover:border-gray-50 cursor-pointer"
+                    >
                       <td className="py-2 px-2 sm:py-4 sm:px-4">
                         <img
                           src={order.orderItems[0].image} alt={order.orderItems[0].name}
